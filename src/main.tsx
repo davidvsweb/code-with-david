554b8ba0490863003d5decd8410780b1f882bb2f
David Vazquez Sanchez <vsjd.99@gmail.com>
2024-10-24 19:16:52 -0600

    fix main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './App.css'
-import { Home } from './components/Home.tsx'
+import { Home } from './components/Home'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Home />,
